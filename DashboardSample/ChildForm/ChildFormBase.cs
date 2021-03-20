e1b68afb01c4dbebef207d595cb4f0836b9074d9
yuuki <proto.atenza@gmail.com>
2021-03-20 23:23:34 +0900

    ベースウォームにNameOf()、WriteLog()追加

--- DashboardSample/ChildForm/ChildFormBase.cs
+++ DashboardSample/ChildForm/ChildFormBase.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DashboardSample.Utility;
 
 namespace DashboardSample.ChildForm
 {
@@ -15,6 +18,8 @@ namespace DashboardSample.ChildForm
     /// </summary>
     public partial class ChildFormBase : Form
     {
+        protected string Log_Key = "UI";
+
         /// <summary>
         /// クラスの新しいインスタンスを構築します。
         /// </summary>
@@ -27,5 +32,26 @@ namespace DashboardSample.ChildForm
         {
             this.Close();
         }
+
+        protected void WriteLog(MethodBase methodBase, string msg)
+        {
+            var outMsg = String.Format("{0},{1},{2}(),{3},{4}",
+                DateTime.Now.ToString("HH:mm:ss.fff"), methodBase.DeclaringType.FullName, methodBase.Name, Log_Key, msg);
+            Console.WriteLine(outMsg);
+        }
+
+        /// <summary>
+        /// ラムダ式によって渡されたメンバ名を取得します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e">ラムダ式</param>
+        /// <returns>メンバ名</returns>
+        #region NameOf
+        protected string NameOf<T>(Expression<Func<T>> e)
+        {
+            return ObjectHelper.NameOf(e);
+        }
+        #endregion NameOf
+
     }
 }
